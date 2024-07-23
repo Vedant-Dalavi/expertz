fc3ec0b06dbdfe5689fe43cf9db0d4bb6cf526c1
SHIVRAJ ARVIND KADAM <128916417+shivrajkadam19@users.noreply.github.com>
2024-07-24 00:47:12 +0530

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -569,7 +569,6 @@ exports.adminLogin = async (req, res, next) => {
             const options = {
                 expiresIn: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
                 httpOnly: true,
-                httpOnly: true,
                 secure: true, // Set to true if using HTTPS
                 sameSite: 'Strict'
             };
