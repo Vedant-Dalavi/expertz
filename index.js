4efdfaf5ef82dff67273278cce8ffc3acf479bba
Vedant Dalavi <154957679+Vedant-Dalavi@users.noreply.github.com>
2024-07-24 17:33:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ database.connect();
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-    cors({
-        origin: "http://localhost:4000",
-        credentials: true,
-    })
-);
+app.use(cors());
+
+// {
+    //     origin: "http://localhost:4000",
+    //     credentials: true,
+    // }
 
 
 
